44026170725c9ed4242b98b311db355e57069dbe
annie <knr9144@icloud.com>
2022-08-14 15:47:47 +0900

    Initial Commit

---
+++ newTMDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  newTMDB
+//
+//  Created by 나리강 on 2022/08/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
